65824d3967066f2c04033673431dde5555000699
RynT <122379612+AaaRynt@users.noreply.github.com>
2026-04-17 20:07:00 +0800

    absolute

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
   },
   i18n: {
     locales: ['zh', 'en'],
